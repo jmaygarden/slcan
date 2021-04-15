f829ea44d3c78e4b1babd34bdc885ef92bb5875a
marcelbuesing <buesing.marcel@googlemail.com>
2021-04-15 21:37:31 +0200

    Add reading and writing of extended frames

--- src/embedded_can_impl.rs
+++ src/embedded_can_impl.rs
@@ -1,17 +1,11 @@
 use crate::{CanFrame, CanSocket};
-use embedded_can::{Can, ExtendedId, Frame, Id, StandardId};
+use embedded_can::{Can, Frame, Id};
 use serial_core::SerialPort;
 use std::io;
 
-const EFF_FLAG: u32 = 0x80000000;
-
 impl Frame for CanFrame {
     fn new(id: impl Into<Id>, data: &[u8]) -> Result<Self, ()> {
-        let id = match id.into() {
-            Id::Standard(id) => id.as_raw().into(),
-            Id::Extended(id) => id.as_raw() | EFF_FLAG,
-        };
-        Ok(CanFrame::new(id, data.len(), data))
+        Ok(CanFrame::new(id.into(), data.len(), data))
     }
 
     fn new_remote(_id: impl Into<Id>, _dlc: usize) -> Result<Self, ()> {
@@ -20,7 +14,7 @@ impl Frame for CanFrame {
     }
 
     fn is_extended(&self) -> bool {
-        self.id & EFF_FLAG != 0
+        matches!(self.id, Id::Extended(_))
     }
 
     fn is_remote_frame(&self) -> bool {
@@ -29,11 +23,7 @@ impl Frame for CanFrame {
     }
 
     fn id(&self) -> Id {
-        if self.id & EFF_FLAG != 0 {
-            Id::Extended(unsafe { ExtendedId::new_unchecked(self.id) })
-        } else {
-            Id::Standard(unsafe { StandardId::new_unchecked(self.id as u16) })
-        }
+        self.id
     }
 
     fn dlc(&self) -> usize {
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,22 @@
 extern crate serial_core as serial;
 
+pub use embedded_can::{ExtendedId, Id, StandardId};
 use serial::prelude::*;
-use std::io;
+use std::{
+    convert::{TryFrom, TryInto},
+    io,
+};
 
-#[cfg(feature = "embedded-can-impl")]
-#[cfg_attr(docsrs, doc(cfg(feature = "embedded-can-impl")))]
 pub mod embedded_can_impl;
 
 // maximum rx buffer len: extended CAN frame with timestamp
 const SLCAN_MTU: usize = "T1111222281122334455667788EA5F\r".len() + 1;
 const SLCAN_CMD_LEN: usize = 1;
-const SLCAN_SDD_ID_LEN: usize = 3;
+const SLCAN_STANDARD_ID_LEN: usize = 3;
+const SLCAN_EXTENDED_ID_LEN: usize = 8;
 
 const BELL: u8 = 0x07;
 const CARRIAGE_RETURN: u8 = '\r' as u8;
-const TRANSMIT_COMMAND: u8 = 't' as u8;
 
 const HEX_LUT: &[u8] = "0123456789ABCDEF".as_bytes();
 
@@ -39,10 +41,32 @@ pub enum Command {
     Open = 'O' as u8,
     /// Close the CAN channel.
     Close = 'C' as u8,
+    /// Transmit a standard (11bit) CAN frame.
+    TransmitStandardFrame = 't' as u8,
+    /// Transmit an extended (29bit) CAN frame.
+    TransmitExtendedFrame = 'T' as u8,
+}
+
+impl TryFrom<u8> for Command {
+    type Error = io::Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value as char {
+            'S' => Ok(Command::Setup),
+            'O' => Ok(Command::Open),
+            'C' => Ok(Command::Close),
+            't' => Ok(Command::TransmitStandardFrame),
+            'T' => Ok(Command::TransmitExtendedFrame),
+            _ => Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Invalid Command",
+            )),
+        }
+    }
 }
 
 pub struct CanFrame {
-    pub id: u32,
+    pub id: Id,
     pub dlc: usize,
     pub data: [u8; 8],
 }
@@ -87,6 +111,21 @@ fn unpack_data(s: &[u8], len: usize) -> Result<[u8; 8], ()> {
     Ok(buf)
 }
 
+fn hextou16(buf: &[u8]) -> Result<u16, ()> {
+    let mut value = 0u16;
+
+    for s in buf.iter() {
+        value <<= 4;
+
+        match hextou8(*s) {
+            Ok(byte) => value |= byte as u16,
+            Err(_) => return Err(()),
+        }
+    }
+
+    Ok(value)
+}
+
 fn hextou32(buf: &[u8]) -> Result<u32, ()> {
     let mut value = 0u32;
 
@@ -106,8 +145,21 @@ fn hexdigit(value: u32) -> u8 {
     HEX_LUT[(value & 0xF) as usize]
 }
 
-fn u32tohex3(value: u32) -> [u8; 3] {
+fn u16tohex3(value: u16) -> [u8; 3] {
     [
+        hexdigit(value as u32 >> 8),
+        hexdigit(value as u32 >> 4),
+        hexdigit(value as u32 >> 0),
+    ]
+}
+
+fn u32tohex8(value: u32) -> [u8; 8] {
+    [
+        hexdigit(value >> 28),
+        hexdigit(value >> 24),
+        hexdigit(value >> 20),
+        hexdigit(value >> 16),
+        hexdigit(value >> 12),
         hexdigit(value >> 8),
         hexdigit(value >> 4),
         hexdigit(value >> 0),
@@ -126,7 +178,7 @@ fn bytestohex(data: &[u8]) -> Vec<u8> {
 }
 
 impl CanFrame {
-    pub fn new(id: u32, dlc: usize, data: &[u8]) -> Self {
+    pub fn new(id: Id, dlc: usize, data: &[u8]) -> Self {
         let mut copy = [u8::default(); 8];
         copy[..data.len()].copy_from_slice(data);
 
@@ -142,7 +194,7 @@ impl std::fmt::Display for CanFrame {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "CanFrame{{ id: {}, dlc: {}, data: {:?} }}",
+            "CanFrame{{ id: {:?}, dlc: {}, data: {:?} }}",
             self.id, self.dlc, self.data
         )
     }
@@ -172,7 +224,7 @@ impl<P: SerialPort> CanSocket<P> {
         Ok(())
     }
 
-    pub fn write(&mut self, id: u32, data: &[u8]) -> io::Result<usize> {
+    pub fn write(&mut self, id: Id, data: &[u8]) -> io::Result<usize> {
         let dlc = data.len();
 
         if dlc > 8 {
@@ -181,8 +233,17 @@ impl<P: SerialPort> CanSocket<P> {
 
         let mut buf = Vec::<u8>::with_capacity(6 + 2 * dlc);
 
-        buf.push('t' as u8);
-        buf.extend_from_slice(&u32tohex3(id));
+        match id {
+            Id::Standard(standard_id) => {
+                buf.push(Command::TransmitStandardFrame as u8);
+                buf.extend_from_slice(&u16tohex3(standard_id.as_raw()));
+            }
+            Id::Extended(extended_id) => {
+                buf.push(Command::TransmitExtendedFrame as u8);
+                buf.extend_from_slice(&u32tohex8(extended_id.as_raw()));
+            }
+        }
+
         buf.push(hexdigit(dlc as u32));
         buf.extend_from_slice(&bytestohex(data));
         buf.push('\r' as u8);
@@ -222,21 +283,57 @@ impl<P: SerialPort> CanSocket<P> {
     }
 
     fn bump(&mut self) -> io::Result<CanFrame> {
-        let cmd = self.rbuff[0];
+        let cmd = self.rbuff[0].try_into();
 
         match cmd {
-            TRANSMIT_COMMAND => {
-                let id =
-                    match hextou32(&self.rbuff[SLCAN_CMD_LEN..SLCAN_CMD_LEN + SLCAN_SDD_ID_LEN]) {
-                        Ok(value) => value,
-                        Err(()) => return Err(io::Error::new(io::ErrorKind::WouldBlock, "")),
-                    };
-                let dlc = (self.rbuff[SLCAN_CMD_LEN + SLCAN_SDD_ID_LEN] - 0x30) as usize;
+            Ok(Command::TransmitStandardFrame) => {
+                let id = match hextou16(
+                    &self.rbuff[SLCAN_CMD_LEN..SLCAN_CMD_LEN + SLCAN_STANDARD_ID_LEN],
+                ) {
+                    Ok(value) => value,
+                    Err(()) => return Err(io::Error::new(io::ErrorKind::WouldBlock, "")),
+                };
+                let dlc = (self.rbuff[SLCAN_CMD_LEN + SLCAN_STANDARD_ID_LEN] - 0x30) as usize;
 
-                if let Ok(data) =
-                    unpack_data(&self.rbuff[SLCAN_CMD_LEN + SLCAN_SDD_ID_LEN + 1..], dlc)
-                {
-                    Ok(CanFrame { id, dlc, data })
+                if let Ok(data) = unpack_data(
+                    &self.rbuff[SLCAN_CMD_LEN + SLCAN_STANDARD_ID_LEN + 1..],
+                    dlc,
+                ) {
+                    let standard_id = StandardId::new(id).ok_or(io::Error::new(
+                        io::ErrorKind::InvalidData,
+                        "Standard id exceeds max size",
+                    ))?;
+                    Ok(CanFrame {
+                        id: Id::Standard(standard_id),
+                        dlc,
+                        data,
+                    })
+                } else {
+                    Err(io::Error::new(io::ErrorKind::InvalidData, ""))
+                }
+            }
+            Ok(Command::TransmitExtendedFrame) => {
+                let id = match hextou32(
+                    &self.rbuff[SLCAN_CMD_LEN..SLCAN_CMD_LEN + SLCAN_EXTENDED_ID_LEN],
+                ) {
+                    Ok(value) => value,
+                    Err(()) => return Err(io::Error::new(io::ErrorKind::WouldBlock, "")),
+                };
+                let dlc = (self.rbuff[SLCAN_CMD_LEN + SLCAN_EXTENDED_ID_LEN] - 0x30) as usize;
+
+                if let Ok(data) = unpack_data(
+                    &self.rbuff[SLCAN_CMD_LEN + SLCAN_EXTENDED_ID_LEN + 1..],
+                    dlc,
+                ) {
+                    let extended_id = ExtendedId::new(id).ok_or(io::Error::new(
+                        io::ErrorKind::InvalidData,
+                        "Extended id exceeds max size",
+                    ))?;
+                    Ok(CanFrame {
+                        id: Id::Extended(extended_id),
+                        dlc,
+                        data,
+                    })
                 } else {
                     Err(io::Error::new(io::ErrorKind::InvalidData, ""))
                 }
